76c71bbdaf5800acb0eb717803642de78a7460b1
faizan <faizansayyed36@gmail.com>
2021-04-06 00:01:06 +0530

    Home screen product listing and Rating component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,5 +14,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister()
+
 
