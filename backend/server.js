99548f5c540d888fae42e3d4b625c1b5b825be82
faizan <faizansayyed36@gmail.com>
2021-04-10 13:18:59 +0530

    Create product controller, User auth & generate token, Authentication middleware, User registration & password hashing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from './routes/userRoutes.js'
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 connectDB();
@@ -11,11 +12,14 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use("/api/products", productRoutes);
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
